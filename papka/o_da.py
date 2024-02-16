47013111a1c5657b9caf9ca260a3585cebc8ec4e
ded <nukanaka63@gmail.com>
2024-02-16 16:30:30 +0400

    proba.krovi

--- papka/o_da.py
+++ papka/o_da.py
@@ -82,15 +82,15 @@ lose = font1.render("You lose!", True, (180, 0, 0))
 
 num_bullets = 5
 
-player = Player('car.png', 25, win_height - 100, 80, 100, 10)
-monster1 = Enemy('wolf.png', randint(0, win_width - 80), 0, 80, 50, randint(2, 7))
-monster2 = Enemy('wolf.png', randint(0, win_width - 80), 0, 80, 50, randint(2, 7))
-monster3 = Enemy('wolf.png', randint(0, win_width - 80), 0, 80, 50, randint(2, 7))
-monster4 = Enemy('wolf.png', randint(0, win_width - 80), 0, 80, 50, randint(2, 7))
-monster5 = Enemy('wolf.png', randint(0, win_width - 80), 0, 80, 50, randint(2, 7))
+player = Player('ship2.png', 25, win_height - 100, 80, 100, 10)
+monster1 = Enemy('who2.png', randint(0, win_width - 80), 0, 80, 50, randint(2, 7))
+monster2 = Enemy('who2.png', randint(0, win_width - 80), 0, 80, 50, randint(2, 7))
+monster3 = Enemy('who2.png', randint(0, win_width - 80), 0, 80, 50, randint(2, 7))
+monster4 = Enemy('who2.png', randint(0, win_width - 80), 0, 80, 50, randint(2, 7))
+monster5 = Enemy('who2.png', randint(0, win_width - 80), 0, 80, 50, randint(2, 7))
 
-asteroid1 = Asteroid('jordan3.png', randint(0, win_width - 80), 0, 80, 50, randint(2, 7))
-asteroid2 = Asteroid('jordan3.png', randint(0, win_width - 80), 0, 80, 50, randint(2, 7))
+asteroid1 = Asteroid('img_1.png', randint(0, win_width - 80), 0, 80, 50, randint(2, 7))
+asteroid2 = Asteroid('img_1.png', randint(0, win_width - 80), 0, 80, 50, randint(2, 7))
 
 monsters = sprite.Group()
 monsters.add(monster1)
@@ -122,7 +122,7 @@ finish = False
 pygame.mixer.init()
 pygame.mixer.music.load('gimn-rossiyskoy-federatsii-so-slovami-natsionalnyiy-2556.ogg')
 pygame.mixer.music.play()
-pygame.fire_sound = pygame.mixer.Sound("fire.ogg")
+pygame.fire_sound = pygame.mixer.Sound("shoot.ogg")
 
 while game:
     for e in pygame.event.get():
@@ -167,17 +167,17 @@ while game:
                 bullet_flag = False
 
         for c in collides:
-            monster = Enemy('wolf.png', randint(80, win_width - 80), -40, 80, 50, randint(2, 6))
+            monster = Enemy('who2.png', randint(80, win_width - 80), -40, 80, 50, randint(2, 6))
             monsters.add(monster)
             score += 1
             text_babylya = font2.render("Счёт:" + str(score), 1, (255, 255, 255))
 
         for c in collides2:
-            monster = Enemy('wolf.png', randint(80, win_width - 80), -40, 80, 50, randint(2, 7))
+            monster = Enemy('who2.png', randint(80, win_width - 80), -40, 80, 50, randint(2, 7))
             monsters.add(monster)
 
         for c in collides3:
-            asteroid = Asteroid('jordan3.png', randint(80, win_width - 80), -40, 80, 50, randint(2, 7))
+            asteroid = Asteroid('img_1.png', randint(80, win_width - 80), -40, 80, 50, randint(2, 7))
             asteroid.add(asteroids)
 
         if sprite.spritecollide(player, monsters, False) or sprite.spritecollide(player, asteroids, False):
